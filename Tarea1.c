c0b68e19adfe69490f527fba127884c53fb75d57
EstebanJCh97 <57113689+EstebanJCh97@users.noreply.github.com>
2022-09-02 21:57:47 -0600

    Update Tarea1.c

--- Tarea1.c
+++ Tarea1.c
@@ -9,22 +9,30 @@ void mostrar_matriz(int dim, int matriz[dim][dim]);
 
 int main(){
  srand(time(NULL));
-    int dim;
+  int dim;
  printf("Ingrese la dimension: ");
  scanf("%d",&dim);
-  int matrizA[dim][dim];
-  int matrizB[dim][dim];
+  int matriz1[dim][dim];
+  int matriz2[dim][dim];
+  int matriz3[dim][dim];
 
  
- llenar_Matriz(dim,matrizA);// esta parte llamo a la funcion
- llenar_Matriz(dim,matrizB);// esta parte llamo a la funcion
+ llenar_Matriz(dim,matriz1);// esta parte llamo a la funcion
+ llenar_Matriz(dim,matriz2);// esta parte llamo a la funcion
+
 
  printf("\nLa matriz A es la siguiente \n");
- mostrar_matriz(dim,matrizA);// esta parte llamo a la funcion para que me muestre la matriz
+ mostrar_matriz(dim,matriz1);// esta parte llamo a la funcion para que me muestre la matriz
  
  printf("\nLa matriz B es la siguiente");
  printf("\n");
- mostrar_matriz(dim,matrizB);// esta parte llamo a la funcion para que me muestre la matriz
+ mostrar_matriz(dim,matriz2);// esta parte llamo a la funcion para que me muestre la matriz
+
+
+ producto_matriz(dim,matriz1,matriz2,matriz3);
+ printf("\nEL resultado del producto matriz es el siguiente");
+ printf("\n");
+ mostrar_matriz(dim,matriz3);// esta parte llamo a la funcion para que me muestre la matriz
  
  return 0;
 }
@@ -52,4 +60,23 @@ void llenar_Matriz(int dim, int matriz[dim][dim]){
     matriz[i][j]= 1+rand() % 9;
   }
  }
+}
+
+int producto_matriz(int dim,int matriz1[dim][dim],int matriz2[dim][dim],int matriz3[dim][dim]){
+
+
+    // Necesitamos hacer esto por cada columna de la segunda matriz (B)
+    for (int a = 0; a < dim; a++) {
+        // Dentro recorremos las filas de la primera (A)
+        for (int i = 0; i < dim; i++) {
+            int suma = 0;
+            // Y cada columna de la primera (A)
+            for (int j = 0; j < dim; j++) {
+                // Multiplicamos y sumamos resultado
+                suma += matriz1[i][j] * matriz2[j][a];
+            }
+            // Lo acomodamos dentro del producto
+            matriz3[i][a] = suma;
+        }
+    }
 }
